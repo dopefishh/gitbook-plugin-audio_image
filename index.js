d321c809835c4ba142c39804c65383ee568a15e9
Mart Lubbers <mart.lubbers@mpi.nl>
2015-07-17 09:49:17 +0200

    v0.0.03

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-// Based on my fiddle http://jsfiddle.net/xgrommx/577pn/5
-function (body){
-	var args = body.split(' ');
-	return 'Number of arguments: ' + args.length;
-}
-
 module.exports = {
 	blocks: {
 		audio_image: {
 			process: function(block) {
-				return makeAudioImage(block.body);
+					var args = block.body.split(' ');
+					return 'Number of arguments: ' + args.length;
+				}
 			}
 		}
 	}
